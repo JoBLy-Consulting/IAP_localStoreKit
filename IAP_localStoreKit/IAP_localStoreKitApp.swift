a216a4ee5a0a6adde40847d0d39e6d3e082617a4
Cityhunter <johan.guenaoui@gmail.com>
2021-12-05 22:50:15 +0100

    Initial Commit

---
+++ IAP_localStoreKit/IAP_localStoreKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  IAP_localStoreKitApp.swift
+//  IAP_localStoreKit
+//
+//  Created by Johan Guenaoui on 05/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct IAP_localStoreKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
